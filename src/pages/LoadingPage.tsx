5bd1588d60262fa8a40a3f2d323d44f848b0617d
jay <harsha.gontu@gmail.com>
2023-03-27 00:08:54 +0530

    main load page

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -3,7 +3,7 @@ import "../css/LoadingPageCss/loading.css";
 const LoadingPage = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-[rgb(7,7,7)]">
-      <h1 className="loadMain font-[Anurati] text-8xl tracking-[0.3em]">
+      <h1 className="loadMain font-[Anurati] text-3xl sm:text-4xl md:text-6xl lg:text-8xl xl:text-8xl 2xl:text-9xl tracking-[0.3em]">
         CINEFLEXA
       </h1>
     </div>
