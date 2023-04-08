d500fd31deddb254a40952fb551ce93247c37905
jay <harsha.gontu@gmail.com>
2023-04-09 01:25:41 +0530

    responsive navbar

--- src/components/navComponents/Navbar.tsx
+++ src/components/navComponents/Navbar.tsx
@@ -1,22 +1,38 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-      <nav className="h-16 w-full fixed bg-transparent overflow-x-hidden overflow-y-hidden z-50">
+      <nav className="h-16 w-full fixed bg-transparent overflow-x-hidden overflow-y-hidden z-50 ">
         <div className="pt-3 pl-1 flex justify-between item-center h-full w-full">
           <div className="">
             <h1 className="loadMain text-white font-[Anurati] pl-3 text-xl sm:text-xl md:text-xl lg:text-2xl xl:text-2xl 2xl:text-2xl tracking-[0.3em]">
               CINEFLEXA
             </h1>
           </div>
-          <div className="flex justify-around w-[25%] text-white font-extralight">
+          <div className="flex justify-around text-white font-thin max-sm:hidden">
             <ul className="flex">
-              <li className="px-3 text-xl">Movies</li>
-              <li className="px-3 text-xl">Series</li>
-              <li className="pl-3 text-xl">News</li>
+              <li className="px-3 text-xl">
+                {" "}
+                <Link to={`/`}> Movies</Link>
+              </li>
+              <li className="px-3 text-xl">
+                {" "}
+                <Link to={`/series`}> Series</Link>
+              </li>
+              <li className="pl-3 text-xl">
+                {" "}
+                <Link to={`/anime`}>Anime</Link>
+              </li>
+              <li className="pl-3 text-xl">
+                {" "}
+                <Link to={`/news`}>News</Link>{" "}
+              </li>
             </ul>
-            <div className="text-xl px-3">Sign in</div>
+            <div className="text-xl px-3">
+              {" "}
+              <Link to={`/signin`}>Sign in</Link>{" "}
+            </div>
           </div>
         </div>
       </nav>
--- src/utils/axios_utils.ts
+++ src/utils/axios_utils.ts
@@ -6,9 +6,7 @@ const client = axios.create({
 
 const TMDB_TOKEN: string = import.meta.env.VITE_APP_TOKEN;
 
-export const request = async ({
-  ...options
-}: AxiosRequestConfig): Promise<AxiosResponse> => {
+export const request = async ({...options}: AxiosRequestConfig): Promise<AxiosResponse> => {
   client.defaults.headers.common.Authorization = `Bearer ${TMDB_TOKEN}`;
   console.log("working");
 
@@ -20,3 +18,5 @@ export const request = async ({
     throw error;
   }
 };
+
+
