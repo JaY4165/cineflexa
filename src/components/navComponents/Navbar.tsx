c2cdc5cba4062546e2ff67f8bf10bc519795fca3
jay <harsha.gontu@gmail.com>
2023-04-09 23:35:47 +0530

    navbar set

--- src/components/navComponents/Navbar.tsx
+++ src/components/navComponents/Navbar.tsx
@@ -1,40 +1,56 @@
 import { Outlet, Link } from "react-router-dom";
+import { BsHeartFill, BsBookmarkFill } from "react-icons/bs";
 
 const Navbar = () => {
   return (
     <>
-      <nav className="h-16 w-full fixed bg-transparent overflow-x-hidden overflow-y-hidden z-50 ">
-        <div className="pt-3 pl-1 flex justify-between item-center h-full w-full">
-          <div className="">
-            <h1 className="loadMain text-white font-[Anurati] pl-3 text-xl sm:text-xl md:text-xl lg:text-2xl xl:text-2xl 2xl:text-2xl tracking-[0.3em]">
-              CINEFLEXA
-            </h1>
+      <nav className="h-16 pl-5 w-full bg-transparent overflow-x-hidden overflow-y-hidden z-50 fixed top-0 backdrop-blur-[8px]">
+        <section className="flex justify-between item-center h-full w-full">
+          <div className="self-center">
+            <Link to={`/`}>
+              <h1 className="text-white font-[Anurati]  text-xl sm:text-xl md:text-xl lg:text-2xl xl:text-2xl 2xl:text-2xl tracking-[0.3em]">
+                CINEFLEXA
+              </h1>
+            </Link>
           </div>
-          <div className="flex justify-around text-white font-thin max-sm:hidden">
-            <ul className="flex">
-              <li className="px-3 text-xl">
-                {" "}
-                <Link to={`/`}> Movies</Link>
-              </li>
-              <li className="px-3 text-xl">
-                {" "}
-                <Link to={`/series`}> Series</Link>
-              </li>
-              <li className="pl-3 text-xl">
-                {" "}
-                <Link to={`/anime`}>Anime</Link>
-              </li>
-              <li className="pl-3 text-xl">
-                {" "}
-                <Link to={`/news`}>News</Link>{" "}
-              </li>
-            </ul>
-            <div className="text-xl px-3">
-              {" "}
-              <Link to={`/signin`}>Sign in</Link>{" "}
-            </div>
+          <div className="font-light font-sans flex items-center">
+            <Link
+              to="/movies"
+              className="pl-5 text-white/80 text-xl hover:text-white hover:duration-300 duration-1000"
+            >
+              Movies
+            </Link>
+            <Link
+              to="/shows"
+              className="pl-5 text-white/80 text-xl hover:text-white hover:duration-300 duration-1000"
+            >
+              Tv Shows
+            </Link>
+            <Link
+              to="/newa"
+              className="pl-5 text-white/80 text-xl hover:text-white hover:duration-300 duration-1000"
+            >
+              News
+            </Link>
+            {/* <Link
+              to="/watchlist"
+              className="pl-5 text-white/80 text-xl hover:text-white hover:duration-300 duration-1000"
+            >
+              <BsBookmarkFill />
+            </Link>
+            <Link
+              to="/favourite"
+              className="pl-5 text-white/80 text-xl hover:text-white hover:duration-300 duration-1000"
+            >
+              <BsHeartFill />
+            </Link> */}
+            <Link to="/login" className="px-5">
+              <button className=" text-white/80 bg-none px-3 py-[4px] rounded-md text-xl font-light border-white/80 border-solid border-[1px] hover:bg-slate-50 hover:text-black hover:duration-500 duration-1000">
+                Login
+              </button>
+            </Link>
           </div>
-        </div>
+        </section>
       </nav>
       <Outlet />
     </>
