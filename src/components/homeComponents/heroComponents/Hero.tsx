2dc2e582d4502075886d90a10e906bb60a279446
jay <harsha.gontu@gmail.com>
2023-04-20 22:49:16 +0530

    banner done

--- src/components/homeComponents/heroComponents/Hero.tsx
+++ src/components/homeComponents/heroComponents/Hero.tsx
@@ -37,7 +37,9 @@ const Hero = ({ heroMovie }: Props) => {
   useEffect(() => {
     const fetchMovieImage = async () => {
       await axios
-        .get(`${imageUrl}${bannerMovie?.poster_path}`)
+        .get(
+          `${imageUrl}${bannerMovie?.backdrop_path || bannerMovie?.poster_path}`
+        )
         .then((res) => {
           console.log(res.config.url);
           setBannerImage(res.config.url);
@@ -53,15 +55,15 @@ const Hero = ({ heroMovie }: Props) => {
     <div className="h-screen w-screen  bg-[rgb(7,7,7)] overflow-x-hidden">
       <div
         className={`justify-center h-full object-cover
-        bg-no-repeat bg-cover bg-center brightness-[0.5] opacity-30`}
+        bg-no-repeat bg-cover bg-center`}
         style={{ backgroundImage: `url(${bannerImage || blade1})` }}
       >
-        <div className="flex flex-col justify-end h-full w-full pb-20 pl-4 md:pl-7 md:pt-52">
-          <h1 className="text-white text-3xl sm:text-3xl md:text-4xl lg:text-6xl pb-4 pl-1 font-mono font-bold ">
+        <div className="flex flex-col justify-end h-full w-full pb-20 pl-4 md:pl-7 md:pt-52 backdrop-brightness-[0.25]">
+          <h1 className="text-white text-3xl sm:text-3xl md:text-4xl lg:text-6xl pb-4 pl-1 font-mono font-bold">
             {bannerMovie?.title || "Blade Runner"}
           </h1>
           <div className="pl-1 md:inline-flex">
-            <div className="inline-flex">     
+            <div className="inline-flex">
               <span>
                 <BsStarFill size={30} color="gold" />
               </span>
@@ -87,18 +89,21 @@ const Hero = ({ heroMovie }: Props) => {
               </span>
               <span className="text-white/75 pt-[0.7rem] pl-6">
                 <ul>
-                  <li className="list-item list-disc">2023</li>
+                  <li className="list-item list-disc">
+                    {bannerMovie?.release_date || "2023"}
+                  </li>
                 </ul>
               </span>
             </div>
           </div>
           <div className="pl-3 md:pl-2">
             <p className="paragraph text-white text-sm pt-5 font-mono font-light max-w-[90%] md:max-w-[65%] lg:max-w-[70%] xl:max-w-[75%]">
-              Thirty years after the events of the first film, a new blade
+              {bannerMovie?.overview ||
+                `Thirty years after the events of the first film, a new blade
               runner, LAPD Officer K, unearths a long-buried secret that has the
               potential to plunge what's left of society into chaos. K's
               discovery leads him on a quest to find Rick Deckard, a former LAPD
-              blade runner who has been missing for 30 years.
+              blade runner who has been missing for 30 years.`}
             </p>
           </div>
         </div>
