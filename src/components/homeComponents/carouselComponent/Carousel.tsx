b8dbcb2d2ba50cda1f4d665333c120bd918077b8
jay <harsha.gontu@gmail.com>
2023-05-04 00:33:14 +0530

    slick slider

--- src/components/homeComponents/carouselComponent/Carousel.tsx
+++ src/components/homeComponents/carouselComponent/Carousel.tsx
@@ -1,41 +1,116 @@
 import Slider from "react-slick";
-
-import React from "react";
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const Carousel = () => {
+  interface mov_img {
+    id: number;
+    src: string;
+    alt: string;
+  }
+
+  const mov_img = [
+    {
+      id: 1,
+      src: "https://images.unsplash.com/photo-1627745193246-1fa1c9404b21?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+      alt: "The world",
+    },
+    {
+      id: 2,
+      src: "https://images.unsplash.com/photo-1631116617822-e100bd7e6e06?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+      alt: "Train",
+    },
+    {
+      id: 3,
+      src: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80",
+      alt: "Laptop",
+    },
+    {
+      id: 4,
+      src: "https://images.unsplash.com/photo-1627745193246-1fa1c9404b21?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+      alt: "The world",
+    },
+    {
+      id: 5,
+      src: "https://images.unsplash.com/photo-1631116617822-e100bd7e6e06?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+      alt: "Train",
+    },
+    {
+      id: 6,
+      src: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80",
+      alt: "Laptop",
+    },
+    {
+      id: 7,
+      src: "https://images.unsplash.com/photo-1627745193246-1fa1c9404b21?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+      alt: "The world",
+    },
+    {
+      id: 8,
+      src: "https://images.unsplash.com/photo-1631116617822-e100bd7e6e06?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+      alt: "Train",
+    },
+    {
+      id: 9,
+      src: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80",
+      alt: "Laptop",
+    },
+  ];
+
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
-    slidesToShow: 3,
+    speed: 500,
+    slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 5000,
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    Infinity: true,
     cssEase: "linear",
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
 
   return (
     <div>
-      <h2>Auto Play</h2>
       <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
+        {mov_img.map((slide) => {
+          return (
+            <div className="p-2" key={slide.id}>
+              <img
+                className="w-auto h-auto object-fill"
+                src={slide.src}
+                alt={slide.alt}
+              />
+            </div>
+          );
+        })}
       </Slider>
     </div>
   );
--- src/components/homeComponents/watchables/Watchables.tsx
+++ src/components/homeComponents/watchables/Watchables.tsx
@@ -1,8 +1,10 @@
+import Carousel from "../carouselComponent/Carousel";
+
 const Watchables = () => {
   return (
     <div className="h-screen w-screen">
-      <div className="flex justify-center pt-14 w-full px-8">
-       
+      <div className="pt-14 w-full px-8 z-50 text-black ">
+        <Carousel />
       </div>
     </div>
   );
